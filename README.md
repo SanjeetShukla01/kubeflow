39af50775c32d0817872f3bd668117e5c3b62110
SanjeetShukla01 <sanjeet.shukla089@gmail.com>
2024-06-16 23:18:55 +0530

    adding readme

---
+++ README.md
@@ -0,0 +1,8 @@
+minikube start
+minikube stop
+
+minikube version
+kubectl get po -A
+kubectl version --client
+kubectl version --client --output=yaml
+kill -9 1509
---
+++ minikube_env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# minishift profile set 9steps
+minikube --profile 9steps config set memory 6144
+minikube --profile 9steps config set cpus 2 (1)
+minikube --profile 9steps config set vm-driver virtualbox #hyperkit (2)
+# kubernetes version only applies to minikube
+minikube --profile 9steps config set kubernetes-version v1.30.2 (3)
+# minishift addon enable admin-user (4)
+# minishift addon enable anyuid (5)
+minikube start --profile 9steps (6)
